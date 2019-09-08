73ddb653328a7419f2c7213ed98b72b5cdb5a943
ays95 <ays95@DESKTOP-UMVUKEU>
2019-09-09 08:14:51 +0900

    2019-09-09-8:14

--- WebContent/gtsm/gtsm.sql
+++ WebContent/gtsm/gtsm.sql
@@ -8,3 +8,15 @@ gtsm_regdate DATETIME NOT NULL
 
 INSERT INTO gtsm(gtsm_seq, gtsm_name, gtsm_email, gtsm_pwd, gtsm_regdate)
 VALUES (NULL,'test','test@test.com',PASSWORD('test'),NOW());
+
+UPDATE gtsm
+SET gtsm_name ='test1', gtsm_email = 'test1@test.com' , gtsm_regdate=NOW()
+WHERE gtsm_seq = 1;
+
+SELECT gtsm_seq, gtsm_name, gtsm_email, gtsm_pwd, gtsm_regdate
+FROM gtsm
+ORDER BY gtsm_seq DESC
+LIMIT 0,100;
+
+DELETE FROM gtsm
+WHERE gtsm_seq =1;
